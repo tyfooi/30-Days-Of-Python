5f6410c203b5448bd3ce03c6385543bd541effe2
tyfooi <monzon.cooper@yahoo.com>
2024-04-30 11:19:28 -0500

    finished 17

---
+++ 17_Day_Exception_handling/17_exceptions.py
@@ -0,0 +1,9 @@
+names = ['Finland', 'Sweden', 'Norway','Denmark','Iceland', 'Estonia','Russia']
+#they really arent having me do exception handling??
+*nordic_countries,es,ru = names
+
+print(nordic_countries)
+print(es)
+print(ru)
+
+# Some of these are really short no?
